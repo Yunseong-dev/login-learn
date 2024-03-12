47d88ec8ebf18e05f62b67cca33057151377eeff
Yunseong Jeong <jyerd333@gmail.com>
2024-03-12 09:21:22 +0900

    renamed

--- front/src/page/signup.tsx
+++ front/src/page/signup.tsx
@@ -38,7 +38,7 @@ const SignUp = () => {
 
   };
 
-  const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+  const signUpHandler = useCallback((e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (password !== repassword) {
       alert('비밀번호가 일치하지 않습니다.');
@@ -49,7 +49,7 @@ const SignUp = () => {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={signUpHandler}>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="이름" />
         <input type="text" value={id} onChange={(e) => setId(e.target.value)} placeholder="아이디" />
         <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="이메일" />
